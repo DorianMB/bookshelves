aeaf2005a624fc5cf3244d7a55f60d43d1f92b42
Dorian MARQUES BRAGA <dorian.marques_braga@devinci.fr>
2019-02-05 16:22:26 +0100

    Books Service

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Book} from '../models/Book.model';
 import {Subject} from 'rxjs';
+import * as firebase from 'firebase';
+import {error} from '@angular/compiler/src/util';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,50 @@ export class BooksService {
 
   emitBooks() {
     this.booksSubject.next(this.books.slice());
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      });
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book){
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if (bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
   }
 }
